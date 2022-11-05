24b57e5e5544fecf5fc88f11fc23baea3f37e540
Pawan-Ambhore <pa1ambhore358@gmail.com>
2022-11-05 15:06:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import AllCameras from './AllCameras';
+// import Camera from './Camera';
+
+function App() {
+  return (
+    <div className="App">
+
+<AllCameras/>
+
+{/* <Camera/> */}
+
+    </div>
+  );
+}
+
+export default App;
